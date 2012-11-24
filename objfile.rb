bea9674e536cada787f4734b368d5e975330273b
WaveformDelta <WaveformDelta@gmail.com>
2012-11-23 23:46:29 -0600

    Remove the testing code from objfile.rb
    objfile is just a library to be required in other scripts now.

--- objfile.rb
+++ objfile.rb
@@ -1,5 +1,5 @@
 #====================================================================
-# readobj: read and write object files
+# ObjFile: Describe an object file
 #
 # In each of the hashes below, there may be extra fields beyond
 # the ones listed
@@ -218,25 +218,4 @@ class ObjFile
 	  rloc[:extra] = extra  # Always include extra, even if it's nil
 	  return rloc
 	end
-end
-
-ofile = ObjFile.new("main.lk")
-
-puts ofile.sourcefile + " ==========================="
-printf("%d segments, %d symbols, %d relocations\n", ofile.nsegs, ofile.nsyms, ofile.nrlocs)
-
-puts "\nSegments: =========================="
-ofile.segrecs.each do |seg|
-  printf("Name: %s, Loc: %x, Size: %x, Type: %s\n", seg[:name], seg[:loc], seg[:size], seg[:type])
-  printf("Data: %s...\n", seg[:data].bin2hex[0..16]) if /P/===seg[:type]
-end
-
-puts "\nSymbols: ==========================="
-ofile.symrecs.each do |sym|
-  printf("Name: %s, Value: %x, Segment num: %d, Type: %s\n", sym[:name], sym[:value], sym[:seg], sym[:type])
-end
-
-puts "\nRelocations: ======================="
-ofile.rlocrecs.each do |rloc|
-  printf("Location: %x, Target segment num: %d, Refers to: %d, Type: %s\n", rloc[:loc], rloc[:seg], rloc[:ref], rloc[:type])
 end
