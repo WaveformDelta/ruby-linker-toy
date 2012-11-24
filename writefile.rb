da13697457daef513b26b3228d6cd0e83a9be0f7
WaveformDelta <WaveformDelta@gmail.com>
2012-11-24 00:20:26 -0600

    Create a script that writes an object file to disk.
    This uses ObjFile.writeobject to write the object to disk.  Input and
    output filenames are specified on the command line.

---
+++ writefile.rb
@@ -0,0 +1,15 @@
+#=========================================================
+# writefile.rb
+#
+# Read an object file and copy it to disk under a
+# different name.  Format is:
+#
+#   ruby writefile.rb <inputfile> <outputfile>
+#=========================================================
+require 'objfile'
+
+exit if ARGV.size < 2
+
+ofile = ObjFile.new(ARGV[0])
+ofile.writeobject(ARGV[1])
+
