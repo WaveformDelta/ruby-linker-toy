787d2484770166603a69eace60ad6c99bc7d7478
Rob Lyman <robl@datalogics.com>
2012-11-25 20:44:24 -0600

    First version of 'linker': combine all segments in output file.
    This version will place all segments it finds into the output
    file, but does not properly merge segments or adjust offsets.
    
    It also does not update the segment count.

---
+++ linkfiles.rb
@@ -0,0 +1,37 @@
+#=========================================================
+# linkfiles.rb
+#
+# Read several object files and combine them into an
+# output file, allocating storage appropriately.
+# Call as:
+#
+#   ruby linkfiles.rb [<inputfile>...] <outputfile>
+#=========================================================
+require 'objfile'
+
+exit if ARGV.size < 2
+
+inputs = []
+
+for i in (0...ARGV.size-1)
+  puts "Creating ObjFile for " + ARGV[i]
+  ofile = ObjFile.new
+  ofile.loadobject(ARGV[i])
+  inputs[i] = ofile
+end
+
+output = ObjFile.new
+
+inputs.each do |object|
+  object.segrecs.each do |seg|
+	output.segrecs << seg
+  end
+end
+
+puts "\nSegments for output: =========================="
+output.segrecs.each do |seg|
+  printf("Name: %s, Loc: %x, Size: %x, Type: %s\n", seg[:name], seg[:loc], seg[:size], seg[:type])
+  printf("Data: %s...\n", seg[:data].bin2hex[0..16]) if /P/===seg[:type]
+end
+
+output.writeobject(ARGV[-1])
